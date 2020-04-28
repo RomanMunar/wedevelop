5ad0251c4e8249728d5243bc01b8c0613d62d2a0
romanmunar <roman.munar1@gmail.com>
2020-04-28 15:45:28 +0800

    Added Profile form with hooks and alerts

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import store from './store';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 import Dashboard from './components/dashboard/Dashboard';
+import ProfileForm from './components/profile-forms/ProfileForm';
 import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 
@@ -31,6 +32,7 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute exact path="/create-profile" component={ProfileForm} />
             </Switch>
           </section>
         </Fragment>
