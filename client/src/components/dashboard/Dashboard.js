44aefca9fe482a050ba91867f2b1cf86f0d12c37
romanmunar <roman.munar1@gmail.com>
2020-05-02 14:42:48 +0800

    Provided '/create-profile' option to new users

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -38,7 +38,12 @@ const Dashboard = ({
           </div>
         </Fragment>
       ) : (
-        <Fragment>no profile</Fragment>
+        <Fragment>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </Fragment>
       )}
     </Fragment>
   );
