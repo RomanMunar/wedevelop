b3581db0e1b2060db5d6e31458f80d3694012843
romanmunar <roman.munar1@gmail.com>
2020-05-03 13:59:49 +0800

    Edited postItem to be compatible with main post

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,19 +1,26 @@
 import React, { useEffect, Fragment } from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
+import PostItem from '../posts/PostItem';
 import { connect } from 'react-redux';
 import { getPost } from '../../actions/post';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
     getPost(match.params.id);
-    console.log(match.params.id)
+    console.log(match.params.id);
   }, [getPost, match.params.id]);
-  return <Fragment>
-    <Link to='/posts'className='btn'>
-      Back To Posts
-    </Link>
-  </Fragment>;
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to="/posts" className="btn">
+        Back To Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+    </Fragment>
+  );
 };
 
 Post.propTypes = {
