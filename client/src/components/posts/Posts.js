dc43eebbcc8143fd844d2d7218af701d6d54ae78
romanmunar <roman.munar1@gmail.com>
2020-05-03 11:46:49 +0800

    Added PostForm  in Post route

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
 import PostItem from './PostItem';
+import PostForm from './PostForm';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
 
@@ -15,6 +16,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
       <p className="lead">
         <i className="fas fa-user" /> Welcome to the community
       </p>
+      <PostForm />
       {loading ? (
         <Spinner />
       ) : (
